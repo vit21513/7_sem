71b95240fb781271d5991b439d8524df85fa4584
«vitaly» <vit21513@yandex.ru>
2022-08-14 11:23:13 +0300

    add task  еуьз

--- task 50/Program.cs
+++ task 50/Program.cs
@@ -45,8 +45,8 @@ int UserInputInt(string userInputStr)
 
 void PrintIndexMatrix(int[,] array, int indStrok, int indexStolb, int indS, int indStr)
 {
-    if (indStrok > indStr || indexStolb > indS) Console.WriteLine("Значения введеного индекса находятся за пределами массива");
-    else if (indStrok <= 0 || indexStolb <= 0) Console.WriteLine("Значения введеного индекса не корректны");
+    if (indStrok > indStr || indexStolb > indS) Console.WriteLine("Такого элемента нет в массиве");
+    else if (indStrok <= 0 || indexStolb <= 0) Console.WriteLine("такого элемента нет в массиве");
     else Console.WriteLine($" Значение числа по введенному индексу составляет {array[indStrok - 1, indexStolb - 1]}");
 }
 

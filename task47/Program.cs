16708ec941b79ceaff539ebb135c92017765a6ba
«vitaly» <vit21513@yandex.ru>
2022-08-14 10:32:17 +0300

    add task 47

---
+++ task47/Program.cs
@@ -0,0 +1,53 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+
+
+double[,] GetMatrix(int m, int n)
+{
+    double[,] matrix = new double[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(rnd.NextDouble() * 10, 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < array.GetLength(1) - 1) Console.Write($"{array[i, j],3}|");
+            else Console.Write($"{array[i, j],3}]");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int UserInputInt(string userInputStr)
+{
+    Console.WriteLine(userInputStr);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+
+
+int m = UserInputInt("Введите   длину строки маcсива ");
+int n = UserInputInt("Введите  длину  столбцов масcива ");
+
+
+
+double[,] matrixResult = GetMatrix(m, n);
+PrintMatrix(matrixResult);
+

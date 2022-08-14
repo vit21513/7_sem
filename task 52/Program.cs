448afb41e9d45fedf7227a4fc853d51968e32514
«vitaly» <vit21513@yandex.ru>
2022-08-14 10:42:35 +0300

    add task 52

---
+++ task 52/Program.cs
@@ -0,0 +1,66 @@
+﻿//Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int UserInputInt(string userInputStr)
+
+{
+    Console.Write(userInputStr);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+
+}
+
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < array.GetLength(1) - 1) Console.Write($"{array[i, j],3},");
+            else Console.Write($"{array[i, j],3}]");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Arithmeticmean(int[,] arr)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            sum = sum + arr[i, j];
+        }
+        Console.Write($"{Math.Round(sum / arr.GetLength(0), 1)} ");
+    }
+}
+
+
+int m = UserInputInt("Введите   длину строки масcива ");
+int n = UserInputInt("Введите  длину  столбцов масcива ");
+int[,] matrixResult = GetMatrix(m, n);
+PrintMatrix(matrixResult);
+Console.WriteLine();
+Arithmeticmean(matrixResult);
